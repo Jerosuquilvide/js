9cca649483b950feda78fa74821e9f0e5685a49d
Jeronimo <jeronimosuquilvide@gmail.com>
2023-01-11 12:36:23 -0300

    cambios de estilos

--- landing-asinc/assets/main.js
+++ landing-asinc/assets/main.js
@@ -26,11 +26,11 @@ async function fetchData(urlApi) {
             
             <div class="group relative">
                 <div 
-                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none ">
                 <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
                 </div>
                 <div class="mt-4 flex justify-between">
-                    <h3 class="text-sm text-gray-700">
+                    <h3 class="text-sm text-gray-700 dark:text-white">
                          <span aria-hidden="true" class="absolute inset-0"></span>
                      ${video.snippet.title}
                 </h3>
